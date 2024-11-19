c9f2b1904f93410ecb33b197c389bcb9c6afa230
Danner Kurtz <kurtz.danner@gmail.com>
2024-11-19 09:06:03 -0300

    feat: Aula 2, passando uma base de dados falsa em formato de array para testar funcionalidades do método get, e com o parâmetro ID

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 import express from 'express';
 
+const posts = [
+  {
+    id: 1,
+    descrição: 'Um lindo pôr do sol na praia',
+    imagem: 'https://example.com/imagens/por-do-sol.jpg',
+  },
+  {
+    id: 2,
+    descrição: 'Gato adorável fazendo careta',
+    imagem: 'https://example.com/imagens/gato-careta.png',
+  },
+  {
+    id: 3,
+    descrição: 'Montanha coberta de neve',
+    imagem: 'https://example.com/imagens/montanha-neve.jpeg',
+  },
+];
+
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('App rodando na porta 3000');
 });
 
-app.get('/api/', (req, res) => {
-  res.status(200).send('Olá mundo!');
+app.get('/posts/', (req, res) => {
+  res.status(200).json(posts);
+});
+
+function buscarPostPorId(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id);
+  });
+}
+
+app.get('/posts/:id', (req, res) => {
+  const index = buscarPostPorId(req.params.id);
+
+  res.status(200).json(posts[index]);
 });
